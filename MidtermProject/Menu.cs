6466d577136a23781e46b618b5d8cf44273e57cb
Kristen Harrell <harrell.kristen@gmail.com>
2020-10-29 14:52:07 -0400

    adding menu class

---
+++ MidtermProject/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MidtermProject
+{
+    class Menu
+    {
+        public void PrintStore()
+        {
+            for (int i = 0; i < Items.Count; i++)
+            {
+                Console.WriteLine($ "Item: {Items[i]} Price: {Price[i]}");
+            }
+        }
+    }
+}
