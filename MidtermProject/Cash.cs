ba3933aec635f059d04aa4b6f69aa11f5bb096b4
synthroot <synthroot@gmail.com>
2020-10-29 20:32:04 -0400

    first commit

---
+++ MidtermProject/Cash.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace MidtermProject
+{
+    class Cash : Payment
+    {
+        public int userCash { get; set; }
+        public Cash(double Amount, int userCash) :base(Amount)
+        {
+            this.userCash = userCash;
+        }
+        public double GetChange()
+        {
+            while (true)
+            {
+                if (userCash >= Amount)
+                {
+                    double change = userCash - Amount;
+                    Console.WriteLine(change);
+                    return change;
+                }
+                if (userCash < Amount)
+                {
+                    Console.WriteLine("Insufficient cash");
+                    continue;
+                } 
+            }
+            //while (userCash <= Amount)
+            //{
+            //    // Payment option question
+            //    if (userCash < Amount)
+            //    {
+            //        Console.WriteLine("Not enough funds. Try a different payment option? [y/n]");
+            //        continue;
+            //    }
+            //    double change = userCash - Amount;
+            //    Console.WriteLine(change);
+            //}
+            
+        }
+    }
+}
--- MidtermProject/Program.cs
+++ MidtermProject/Program.cs
@@ -6,7 +6,27 @@ namespace MidtermProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Midterm Project");
+            double amount = 55.50;
+
+            //int checkNumber = int.Parse(GetUserInput("Please input your check number"));
+            //Check check1 = new Check(amount, checkNumber);
+            //check1.PayWithCheck();
+
+            //int cash = int.Parse(GetUserInput("How much are you paying with in cash?"));
+            //Cash cash1 = new Cash(amount, cash);
+            //cash1.GetChange();
+
+            string cardNumber = GetUserInput("Input your card number");
+            string expiration = GetUserInput("Input the expiration date [MM/YY]");
+            int securityCw = int.Parse(GetUserInput("Input the security number (CW)"));
+            Credit credit1 = new Credit(amount, cardNumber, expiration, securityCw);
+            credit1.PayWithCredit();
+        }
+        public static string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            string UserInput = Console.ReadLine();
+            return UserInput;
         }
     }
 }
